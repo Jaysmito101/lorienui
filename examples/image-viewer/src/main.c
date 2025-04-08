56bd7c3ada0428a538fee40bdde9e0f944890847
Jaysmito Mukherjee <jaysmito101@gmail.com>
2025-04-08 23:12:03 +0530

    feat: basic project setup

---
+++ examples/image-viewer/src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "lorien/lorien.h"
+#include "lorien/platforms/any-glfw-opengl3/platform.h"
+
+int main() {
+    printf("Hello, Image Viewer!\n");
+    return 0;
+}
---
+++ lorien/src/lorien.c
@@ -0,0 +1,5 @@
+#include "lorien/lorien.h"
+
+int add(int a, int b) {
+    return a + b;
+}
